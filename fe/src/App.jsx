b271734625b2b4fd0887463a2afd5984c1525058
hoangquan1011 <163086939+hoangquan1011@users.noreply.github.com>
2026-07-03 14:05:46 +0700

    fix

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './component/Header/Header';
 import HeroSection from './component/HeroSection/HeroSection';
-import Collections from './component/Collections/Collections';
 import WhyChoose from './component/WhyChoose/WhyChoose';
 import BuySection from './component/BuySection/BuySection';
 import FeatureOne from './component/FeatureOne/FeatureOne';
